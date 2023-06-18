2733702f211dbe51684f0870612ff24cb0e33d90
neviko <nevosayag@gmail.com>
2023-06-18 09:24:38 +0300

    creates image folder on runtime

--- src/services/expiration-worker.service.ts
+++ src/services/expiration-worker.service.ts
@@ -1,8 +1,17 @@
 import { parentPort } from "worker_threads";
 import { IExpirationPair } from "../common/interfaces/expiration-pair";
 import fs from "fs";
+import path from "path";
 
 const expirationMap = new Map<string, Date>();
+
+// creates an image folder if noe exists
+fs.mkdir(path.join(__dirname, "../../image"), (err) => {
+  if (err) {
+    return console.error(err);
+  }
+  console.log("Directory created successfully!");
+});
 
 parentPort?.on("message", ({ imageId, expirationDate }: IExpirationPair) => {
   expirationMap.set(imageId, expirationDate);
